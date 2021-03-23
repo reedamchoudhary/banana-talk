73cc14c57486740daa4226a3636adb7f1e7a37ec
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-03-23 22:34:41 +0530

    text output done

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
-
-console.log(txtInput)
+var outputText = document.querySelector("#output");
 function clickHandler(){
-    console.log("new clicked!");
-    console.log("input was-->", txtInput.value);
+    outputText.innerText = "jgsbhkjgnje "+txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler);
